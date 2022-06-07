f35d85ccf179f3484454818d68ed9e60062a46a1
santosh33 <santosh33@gmail.com>
2022-06-07 22:06:14 +0530

    pass props for images

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import { faker } from '@faker-js/faker';
 import CommentDetail from "./CommentDetail";
 
 function App() {
@@ -8,16 +9,19 @@ function App() {
         author="Sam"
         timeAgo="Today at 4:45PM"
         content="Nice blog post!"
+        avatar={faker.image.avatar()}
       />
       <CommentDetail
         author="Jane"
         timeAgo="Today at 2:00PM"
         content="Completed task"
+        avatar={faker.image.avatar()}
       />
       <CommentDetail
         author="Alex"
         timeAgo="Yesterday at 5:00PM"
         content="Conference call"
+        avatar={faker.image.avatar()}
       />
     </div>
   );
